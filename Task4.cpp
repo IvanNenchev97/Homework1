426c35710d262b36ae9d5dc4e7e2d29c43e5dc1d
IvanNenchev97 <33345017+IvanNenchev97@users.noreply.github.com>
2017-11-04 14:42:00 +0200

    Add files via upload

---
+++ Task4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int number,i;
+	cout<<"Enter number:";
+	cin>>number;
+	for(i=1;i<=number;i++)
+	{
+		if(number%i==0)
+		{
+			cout<<i<<endl;
+		}
+	}
+	
+	
+	
+
+}
