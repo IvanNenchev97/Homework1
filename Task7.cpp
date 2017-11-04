761a370d5a39708455cb2ea41d2da2e2e7e58ffd
IvanNenchev97 <33345017+IvanNenchev97@users.noreply.github.com>
2017-11-04 15:12:07 +0200

    Add files via upload

---
+++ Task7.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,i;
+	cout<<"Enter number:";
+	cin>>n;
+	for(i=n;i>=1;i--)
+	{
+		if(i%7==0)
+		{
+			cout<<i<<endl;
+		}
+	}
+
+
+	
+	
+	
+	
+
+}
